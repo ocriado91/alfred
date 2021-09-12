df75cc9b5692cc92682ec50861630277e130f1b1
ocriado91 <ocriado91@gmail.com>
2021-09-12 20:20:59 +0200

    Added pytest + UTs + measure coverage through Github Actions pipelines

---
+++ test/test_alfred.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import pytest
+import alfredBot
+from TelegramBot.telegrambot import TelegramBot
+import sys
+
+alfred = alfredBot.alfredBot('no_delivery/alfred.toml')
+
+def test_alfred_get_api_list():
+    assert list(alfred.get_API_list()) == ['GoogleTasks', 
+                                           'GoogleCalendar', 
+                                           'Github']
+
+def test_processingMessage():
+    message = 'Check tasks'
+    aphi_phrases = alfred.get_API_keyphrase()
+
+    # Check if the message is in the list of API keyphrases
+    assert message in aphi_phrases
+
+def test_read_config():
+    config = alfred.read_config()
+    assert config['Miscellaneous']['TESTING']
+
+
+
