c20851ef53fcd92ad806199e1955d1e0dd8e81dc
ocriado91 <ocriado91@gmail.com>
2021-09-18 13:23:56 +0200

    Rename Alfred bot module + Improvement of Linter metrics

--- alfred_bot.py
+++ alfred_bot.py
@@ -3,13 +3,14 @@
 """ Alfred. A (Telegram Bot) Personal Assistant
 """
 
-from TelegramBot.telegrambot import TelegramBot
-
-import toml
 import argparse
+import importlib
 import logging
 import sys
-import importlib
+import toml
+
+from TelegramBot.telegrambot import TelegramBot
+
 
 # Configure logging
 LOG_FORMATTER = '%(asctime)s - %(levelname)s - %(module)s - %(funcName)s - %(message)s'
@@ -21,7 +22,8 @@ logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 
-class alfredBot():
+class AlfredBot():
+    ''' Alfred Bot main class '''
 
     def __init__(self,
                  configfile: str):
@@ -29,23 +31,23 @@ class alfredBot():
         try:
             logger.debug('Starting AlfredBot')
             # Read Alfred Bot configuration file
-            self.config = self.read_config(configfile)
+            self.config = read_config(configfile)
 
             # Init Telegram Bot class
             telegram_config = self.config['TelegramBot']
             self.telegrambot = TelegramBot(telegram_config)
 
-            # Only execute Telegram Bot polling if testing 
+            # Only execute Telegram Bot polling if testing
             # is not enabled into configile
             if not self.config['Miscellaneous']['TESTING']:
-                self.telegramPolling()
+                self.telegram_polling()
 
         except KeyboardInterrupt:
             self.telegrambot.write_message('See you soon! :)')
             logger.info('Catched KeyboardInterrupt. Bye :)')
             sys.exit()
 
-    def telegramPolling(self):
+    def telegram_polling(self):
         ''' Read new Telegram messages through
             pollin mechanism'''
         last_message_id = -1
@@ -57,37 +59,28 @@ class alfredBot():
             if message_id:
                 if message_id != last_message_id:
                     message = self.telegrambot.read_message()
-                    self.processIncomingMessage(message)
+                    self.processing_incoming_message(message)
                     last_message_id = message_id
             else:
                 break
 
-
-    def read_config(self,
-                    configfile: str = None):
-        config = toml.load(configfile)
-        # Disable testing if Miscellaneus section 
-        # is not defined as key into configuration dictionary
-        if 'Miscellaneous' not in config.keys():
-            config['Miscellaneous'] = {'TESTING': False}
-        return config
-
-    def get_API_keyphrase(self):
+    def get_api_keyphrase(self):
+        ''' Get list of API keyphrases '''
 
         phrases = []
         # Extract API packages defined into configuration file
         packages = self.config['API'].keys()
-        logger.debug(f'API packages: {packages}')
+        logger.debug('API packages %s', packages)
         for package in packages:
 
             # Extract API modules from each API package
             modules = self.config['API'][package].keys()
-            logger.debug(f'API modules: {modules}')
+            logger.debug('API modules %s', modules)
             for module in modules:
 
                 # Extract API actions from each API modules
                 actions = self.config['API'][package][module].keys()
-                logger.debug(f'Actions: {actions}')
+                logger.debug('Actions %s', actions)
                 for action in actions:
                     # Try to extract phrase from API action configuration
                     try:
@@ -97,11 +90,11 @@ class alfredBot():
                         continue
         return phrases
 
-    def getAPIFunction(self, phrase: str):
+    def get_api_function(self, phrase: str):
         ''' Get API and function through phrase'''
 
         packages = self.config['API'].keys()
-        logger.debug(f'API packages: {packages}')
+        logger.debug('API packages %s', packages)
         for package in packages:
             modules = self.config['API'][package].keys()
             for module in modules:
@@ -112,34 +105,48 @@ class alfredBot():
                             return f'API.{package}.{module}'
                     except (KeyError, TypeError):
                         continue
+        return None
 
-    def processIncomingMessage(self,
-                               message: str):
+    def processing_incoming_message(self,
+                                    message: str):
 
         ''' Check incoming message and compare against
             phrases defined by configuration'''
 
-        logger.info(f'Received message: {message}')
-        api_phrases = self.get_API_keyphrase()
+        logger.info('Received message: %s', message)
+        api_phrases = self.get_api_keyphrase()
         # Log list of API keyphrases
-        logger.debug(f'API keyphrases: {api_phrases}')
+        logger.debug('API keyphrase %s', api_phrases)
         if message in api_phrases:
 
             # Get API and function through message
-            module_name = self.getAPIFunction(message)
-            logger.debug(f'Module name: {module_name}')
+            module_name = self.get_api_function(message)
+            logger.debu('Module name %s', module_name)
             module = importlib.import_module(module_name)
             class_name = module_name.split('.')[-1]
             dynamic_class = getattr(module, class_name)
             result = dynamic_class(self.config).process_action(message)
-            logger.debug(f'Result: {result}')
+            logger.debug('Result: %s', result)
             self.telegrambot.write_message(result)
         else:
-            logger.info(f'No API phrase found for {message}')
-            self.telegrambot.write_message(f"I don't have any action for {message}. Please, try again")
+            logger.info('No API phrase found for %s', message)
+            self.telegrambot.write_message(f"I don't have any action for {message}.\
+                                             Please, try again")
+
+
+def read_config(configfile: str = None):
+    ''' Read Alfred TOML configuration file '''
+
+    config = toml.load(configfile)
+    # Disable testing if Miscellaneus section
+    # is not defined as key into configuration dictionary
+    if 'Miscellaneous' not in config.keys():
+        config['Miscellaneous'] = {'TESTING': False}
+    return config
 
 
 def argument_parser():
+    ''' Argument parser '''
 
     args = argparse.ArgumentParser()
     args.add_argument('--config',
@@ -150,12 +157,13 @@ def argument_parser():
 
 
 def main():
+    ''' Main function '''
 
     # Argument parser
     args = argument_parser()
 
     # Launch AlfredBot
-    alfredBot(args.config)
+    AlfredBot(args.config)
 
 
 if __name__ == '__main__':
