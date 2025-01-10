0f644cee2c5d7daf2294e31c34cc5208de7c0803
Óscar Criado de la Torre <ocriado91@gmail.com>
2025-01-10 10:48:12 +0000

    Remove local Telegrambot version to use Github package + Fix ruff issues

--- API/Github/github_wrapper.py
+++ API/Github/github_wrapper.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-''' Github Class wrapper through PyGithub'''
+"""Github Class wrapper through PyGithub."""
 
 import logging
 import sys
@@ -12,47 +12,40 @@ logger = logging.getLogger(__name__)
 
 
 class Github(API):
-    ''' Wrapper Github class of PyGithub '''
-    def __init__(self,
-                 config: dict):
+    """Wrapper Github class of PyGithub."""
 
-        logger.info('Starting Github API')
-        github_api_key = config['Common']['API_KEY']
+    def __init__(self, config: dict):
+        """Initialize Github Wrapper class."""
+        logger.info("Starting Github API")
+        github_api_key = config["Common"]["API_KEY"]
         self.github = github.Github(github_api_key)
         self.commit_date = None
 
     def get_last_commit_date(self):
-        ''' Extract date of last commit '''
+        """Extract date of last commit."""
         commits = []
         repos = self.github.get_user().get_repos()
         for repo in repos:
-            branch = repo.get_branch('main')
+            branch = repo.get_branch("main")
             commit = branch.commit
             commits.append(commit)
 
         # Extract last commit
         self.commit_date = commits[-1].commit.author.date
 
-    def process_action(self,
-                       message: str):
-        ''' Process action '''
+    def process_action(self, message: str):
+        """Process action."""
         pass
 
 
-def main():
-    ''' Main function '''
-
+if __name__ == "__main__":
     configpath = sys.argv[1]
-    with open(configpath, 'rb') as config_file:
+    with open(configpath, "rb") as config_file:
         config = tomli.load(config_file)
-    config_github = config['API']['Github']
+    config_github = config["API"]["Github"]
 
     # Init Github API
     github_ = Github(config_github)
 
     # Get last commit
     github_.get_last_commit_date()
-
-
-if __name__ == '__main__':
-    main()
--- API/api_abstract.py
+++ API/api_abstract.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
-''' Abstract class to implement Facade Design Pattern
-    into API interfaces
-'''
+"""Abstract class to implement Facade Design Pattern into API interfaces."""
 
 from abc import ABC, abstractmethod
 
 
 class API(ABC):
-    ''' Abstract class '''
+    """Abstract class."""
+
     @abstractmethod
-    def process_action(self,
-                       message: str):
-        ''' Process incoming message action'''
+    def process_action(self, message: str):
+        """Process incoming message action."""
         pass
